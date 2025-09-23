4416b24f0c4f423a026f8f7f97fcfd83e2c86320
jd119630 <keya.wong@my.jcu.edu.au>
2025-09-23 20:19:47 +1000

    Add reflection answers

---
+++ prac_02/reflection.py
@@ -0,0 +1,30 @@
+"""
+CP1404 Practical Reflection
+"""
+
+"""What percentage of the lecture videos have you watched so far?"""
+# 100% RAHHHH!!!!!
+
+"""What time, location and duration have you scheduled to watch lecture videos?"""
+# At home, full day monday for pre-recorded lectures.
+# Generally hope to finish between 12-3pm but tend to do it at night :/
+
+"""What are you doing well in the subject so far that you will keep doing?"""
+# Probably just completing content (eventually).
+# Organised enough that I'll be finished before the next week starts.
+
+"""What do you need to stop or change in your work for this subject overall so far?"""
+# Stop leaving pre-recorded lectures to the last minute.
+# Generally don't finish them in time before the seminar and my practical is right after,
+# so I'm doing the work with knowledge gaps. (Though I usually fix this after)
+
+"""What are you currently doing poorly for practicals in this subject that you will change?"""
+# I'm rather inefficient in practicals because of the setting.
+# Though mostly just getting interrupted either by people talking to me or other noise.
+# Then I'm distracted and takes a while to refocus.
+# I should probably bring noise-cancelling headphones or something, put my phone on silent, etc.
+
+"""What are you doing really well for practicals in this subject?"""
+# Spending time after practicals to look back at my code and compare to solutions.
+# Usually updating or fixing anything where relevant to improve my code.
+
