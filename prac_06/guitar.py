e69f693f51634623b9e0f8814463c59ca32f7176
jd119630 <keya.wong@my.jcu.edu.au>
2025-10-28 16:14:05 +1000

    Complete Guitar class

---
+++ prac_06/guitar.py
@@ -0,0 +1,26 @@
+"""Guitar Class."""
+CURRENT_YEAR = 2022
+VINTAGE_MINIMUM_YEAR = 50
+
+
+class Guitar:
+    """Guitar class."""
+
+    def __init__(self, name="", year=0, cost=0.00, ):
+        """Initialize a Guitar object."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return string representation of data in a Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
+
+    def get_age(self):
+        """gets age of Guitar."""
+        guitar_age = CURRENT_YEAR - self.year
+        return guitar_age
+
+    def is_vintage(self):
+        """checks if Guitar is vintage."""
+        return self.get_age() >= VINTAGE_MINIMUM_YEAR
