1ac47f5ec990b23d7c29502808fb3f73e34d7348
jd119630 <keya.wong@my.jcu.edu.au>
2025-11-09 17:56:07 +1000

    Complete Project class

---
+++ prac_07/project.py
@@ -0,0 +1,22 @@
+"""Project Class."""
+
+
+class Project:
+    """Project class."""
+
+    def __init__(self, name, date, priority, cost, completion):
+        """Initialize a Project object."""
+        self.name = name
+        self.date = date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
+
+    def __str__(self):
+        """Return string representation of data in a Project."""
+        return (f"{self.name}, start: {self.date}, priority{self.priority}, estimate ${self.cost:2f}, "
+                f"completion: {self.completion}%")
+
+    def __repr__(self):
+        return str(self)
+
