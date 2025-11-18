2aaf817ee5bfeb92ac83c41ec44331bc6595c0fa
jd119630 <keya.wong@my.jcu.edu.au>
2025-11-18 11:11:35 +1000

    Complete walkthrough exercises for Taxi class

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,17 @@
+"""
+Test cases for the Taxi class
+"""
+from prac_09.taxy import Taxi
+
+
+def main():
+    """Tests for Taxi class."""
+    my_taxi = Taxi("Prius 1", 100)
+    print(f"{my_taxi.name} drove {my_taxi.drive(40)}.")
+    print(my_taxi)
+    my_taxi.start_fare()
+    print(f"{my_taxi.name} drove {my_taxi.drive(100)}.")
+    print(my_taxi)
+
+
+main()
