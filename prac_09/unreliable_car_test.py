783a695638c44838f67558a0e8052a325d980238
jd119630 <keya.wong@my.jcu.edu.au>
2025-11-18 13:41:59 +1000

    Refactor max number of runs as a constant in unreliable_car_test

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -3,19 +3,21 @@ Test cases for UnreliableCar
 """
 from prac_09.unreliable_car import UnreliableCar
 
+MAX_NUMBER_OF_RUNS = 10
+
 
 def main():
     """Test UnreliableCars."""
     unreliable_car1 = UnreliableCar(name="Bad car :(", fuel=100, reliability=30)
-    successful_drive = 0
-    for i in range(1, 10):
+    number_of_successful_drives = 0
+    for i in range(1, MAX_NUMBER_OF_RUNS):
         if unreliable_car1.drive(10):
             # when car fails to drive it returns 0, which can be treated as a boolean.
-            # But is it ok to use it as a boolean when it's meant to act as an int?
-            successful_drive += 1
+            number_of_successful_drives += 1
 
-    print(f"In 10 runs, {unreliable_car1.name} successfully drove {successful_drive} times with a reliability of "
-          f"{unreliable_car1.reliability}")
+    print(
+        f"In {MAX_NUMBER_OF_RUNS} runs, {unreliable_car1.name} successfully drove {number_of_successful_drives} times with a reliability of "
+        f"{unreliable_car1.reliability}")
 
 
 main()
